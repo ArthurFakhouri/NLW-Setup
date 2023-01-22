7a84b0264b5a3cb8b66038798f401d375d6e69ac
ArthurFakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2023-01-22 17:34:50 -0300

    finishing our mobile application

---
+++ mobile/src/components/Checkbox.tsx
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+
+interface CheckboxProps extends TouchableOpacityProps {
+    title: string;
+    checked?: boolean;
+}
+
+export function Checkbox({ title, checked = false, ...rest }: CheckboxProps) {
+    return (
+        <TouchableOpacity activeOpacity={.7} {...rest}
+            className="flex-row mb-2 items-center">
+            {
+                checked ?
+                    <Animated.View
+                        className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+                        entering={ZoomIn} exiting={ZoomOut}>
+                        <Feather
+                            name="check"
+                            size={20}
+                            color={colors.white}
+                        />
+                    </Animated.View>
+                    : <View className="h-8 w-8 bg-zinc-800 rounded-lg" />
+            }
+            <Text className="text-white text-base ml-3 font-semibold">
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,99 @@
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import dayjs from "dayjs";
+import { useCallback, useState } from "react";
+import { View, Text, ScrollView, Alert } from "react-native";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { api } from "../lib/axios";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+
+const weekDays = [
+    { abreviacao: 'D', dia: 'Domingo' },
+    { abreviacao: 'S', dia: 'Segunda' },
+    { abreviacao: 'T', dia: 'Terça' },
+    { abreviacao: 'Q', dia: 'Quarta' },
+    { abreviacao: 'Q', dia: 'Quinta' },
+    { abreviacao: 'S', dia: 'Sexta' },
+    { abreviacao: 'S', dia: 'Sábado' },
+]
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
+export function Home() {
+
+    const { navigate } = useNavigation();
+    const [loading, setLoading] = useState(true);
+    const [summary, setSummary] = useState<Summary | null>(null);
+
+    async function fetchData() {
+        try {
+            setLoading(true);
+            const res = await api.get('summary');
+
+            setSummary(res.data);
+        } catch (err) {
+            Alert.alert('Ops', 'Não foi possível carregar o resumo de hábitos.');
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchData();
+    }, []))
+
+    if (loading) {
+        return <Loading />
+    }
+
+    return (
+        <View className="flex-1 bg-background px-8 pt-16">
+            <Header />
+            <View className="flex-row mt-6 mb-2">
+                {
+                    weekDays.map(weekDay => (
+                        <Text key={weekDay.dia} className="text-zinc-400 text-xl font-bold text-center mx-1"
+                            style={{ width: DAY_SIZE, height: DAY_SIZE }}>
+                            {weekDay.abreviacao}
+                        </Text>
+                    ))
+                }
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}>
+                {summary ? <View className="flex-row flex-wrap">
+                    {
+                        datesFromYearStart.map(date => {
+                            const dayHabits = summary.find(day => {
+                                return dayjs(date).isSame(day.date, 'day')
+                            })
+
+                            return (
+                                <HabitDay key={date.toString()}
+                                date={date} amountOfHabit={dayHabits?.amount} amountCompleted={dayHabits?.completed}
+                                    onPress={() => navigate('habit', { date: date.toString() })} />
+                            )
+                        })
+                    }
+                    {
+                        amountOfDaysToFill > 0 ?
+                            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+                                <View key={index} className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                    style={{ width: DAY_SIZE, height: DAY_SIZE }} />
+                            )) : null
+                    }
+                </View> : null}
+            </ScrollView>
+        </View>
+    )
+}
